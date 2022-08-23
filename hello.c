8f05a42ccb2808bb0d989c66edbf037c406ed8ed
Devdha <donghun.dev@gmail.com>
2022-08-24 05:53:11 +0900

    test: :test_tube: test 0

--- hello.c
+++ hello.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h>
 
-char *ft_substr(char const *s, unsigned int start, size_t len) {
-  size_t s_len;
-  char  *ret;
-  s_len = strlen(s);
-  if (s_len <= start || len == 0 || s_len == 0) return ((char *)calloc(1, 1));
-  if (len > s_len - start) len = s_len - start;
-  ret = (char *)malloc(sizeof(char) * (len + 1));
-  if (ret == 0) return (0);
-  strncpy(ret, s + start, len + 1);
-  return (ret);
+#if defined __WINDOWS__
+#define DELIMITER '\\'
+#elif defined __APPLE__
+#define DELIMITER '/'
+#elif defined __linux__
+#define DELIMITER '/'
+#endif
+
+char* ft_get_filename(const char* path) {
+  char* filename = NULL;
+  char* last_delimiter = NULL;
+  if (path == NULL) {
+    return NULL;
+  }
+  last_delimiter = strrchr(path, DELIMITER);
+  if (last_delimiter == NULL) {
+    return NULL;
+  }
+  filename = malloc(strlen(last_delimiter) + 1);
+  if (filename == NULL) {
+    return NULL;
+  }
+  strcpy(filename, last_delimiter + 1);
+  return filename;
 }
 
 int main() {
-  const char *a = "c:\\abc\\bbb\\ccc";
-  size_t      s = strlen(a);
-  printf("origin: %s\n", a);
-  while (s > 0) {
-    if (a[s] == '\\') printf("%s\n", ft_substr(a, s + 1, strlen(a) - s));
-    s--;
-  }
+  char* filename = ft_get_filename("/Users/username/Desktop/test.c");
+  char* win_name = ft_get_filename("C:\\Users\\username\\Desktop\\test.c");
+  printf("%s\n", filename);
+  printf("%s\n", win_name);
+  free(filename);
+  free(win_name);
 }
