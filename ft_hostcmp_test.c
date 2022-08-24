a2890daf7511aac31c94ec7bfe80b69c3092f9ed
jaham <jaham@jahamui-MacBookPro.local>
2022-08-24 15:35:02 +0900

    32_little_done

--- ft_hostcmp_test.c
+++ ft_hostcmp_test.c
@@ -11,22 +11,25 @@ void check(size_t a, size_t b, int expect_same) {
 #include <stdio.h>
 int main() {
 
-    check(8080, htonl(8080), 1);
-    check(8080, 8080, 0);
-    check(htonl(8080), 8080, 1);
-    check(0, 0, 1);
-printf("hee\n");
-    check(UINT32_MAX, UINT32_MAX, 0);
-printf("hee\n");
-    check(5, 5, 0);
-    check(8080, 0x901f000000000000, 0);
-    #undef FT_USE_32_BIT
-    #undef FT_USE_LITTLE_ENDIAN
+    // #define FT_USE_32_BIT
+    // #define FT_USE_LITTLE_ENDIAN
 
+    // check(8080, htonl(8080), 1);
+    // check(8080, 8080, 0);
+    // check(htonl(8080), 8080, 1);
+    // check(0, 0, 1);
+    // check(UINT32_MAX, UINT32_MAX, 1);
+    // check(5, 5, 0);
+    // check(8080, 0x901f000000000000, 0);
+    // #undef FT_USE_32_BIT
+    // #undef FT_USE_LITTLE_ENDIAN
+
+printf("hee\n");
     #define FT_USE_64_BIT
     #define FT_USE_LITTLE_ENDIAN
 
     check(8080, htonl(8080), 0);
+printf("hee\n");
     check(8080, 8080, 0);
     check(htonl(8080), 8080, 0);
     check(0, 0, 1);
