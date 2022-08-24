7154a91694f5651d81dfe24f91d5db86ed45668a
jaham <jaham@jahamui-MacBookPro.local>
2022-08-24 15:45:02 +0900

    seperate_main

--- tester/ft_hostcmp_test.c
+++ tester/ft_hostcmp_test.c
@@ -1,35 +1,37 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+void check(size_t a, size_t b, int expect_same);
 
+#if defined(TEST1)
+
+#define FT_USE_32_BIT
+#define FT_USE_LITTLE_ENDIAN
 #include "FT_HOSTCMP.h"
 
-void check(size_t a, size_t b, int expect_same) {
-    if ((expect_same && (FT_HOSTCMP(a, b) != 0)) || (!expect_same && (FT_HOSTCMP(a, b) == 0)))
-        exit(EXIT_FAILURE);
-}
-#include <stdio.h>
 int main() {
 
-    // #define FT_USE_32_BIT
-    // #define FT_USE_LITTLE_ENDIAN
 
-    // check(8080, htonl(8080), 1);
-    // check(8080, 8080, 0);
-    // check(htonl(8080), 8080, 1);
-    // check(0, 0, 1);
-    // check(UINT32_MAX, UINT32_MAX, 1);
-    // check(5, 5, 0);
-    // check(8080, 0x901f000000000000, 0);
-    // #undef FT_USE_32_BIT
-    // #undef FT_USE_LITTLE_ENDIAN
+    check(8080, htonl(8080), 1);
+    check(8080, 8080, 0);
+    check(htonl(8080), 8080, 1);
+    check(0, 0, 1);
+    check(UINT32_MAX, UINT32_MAX, 1);
+    check(5, 5, 0);
+    check(8080, 0x901f000000000000, 0);
+    #undef FT_USE_32_BIT
+    #undef FT_USE_LITTLE_ENDIAN
 
-printf("hee\n");
+    return 0;
+}
+#endif
+
+#if defined(TEST2)
     #define FT_USE_64_BIT
     #define FT_USE_LITTLE_ENDIAN
 
     check(8080, htonl(8080), 0);
-printf("hee\n");
     check(8080, 8080, 0);
     check(htonl(8080), 8080, 0);
     check(0, 0, 1);
@@ -37,8 +39,6 @@ printf("hee\n");
     check(5, 5, 0);
     check(8080, 0x901f000000000000, 1);
 
-    #undef FT_USE_64_BIT
-    #undef FT_USE_LITTLE_ENDIAN
 
     #define FT_USE_32_BIT
     #define FT_USE_BIG_ENDIAN
@@ -67,4 +67,10 @@ printf("hee\n");
 
     #undef FT_USE_64_BIT
     #undef FT_USE_BIG_ENDIAN
+}
+#endif
+
+void check(size_t a, size_t b, int expect_same) {
+    if ((expect_same && (FT_HOSTCMP(a, b) != 0)) || (!expect_same && (FT_HOSTCMP(a, b) == 0)))
+        exit(EXIT_FAILURE);
 }
